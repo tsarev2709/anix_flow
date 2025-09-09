6da2414e71a0a2fe39370c0c89ce44d144a96819
ivankhk <kukharuk.ia@phystech.edu>
2025-09-09 10:44:22 +0300

    Обновление фронтенда

--- frontend/src/components/ui/slider.jsx
+++ frontend/src/components/ui/slider.jsx
@@ -9,12 +9,11 @@ const Slider = React.forwardRef(({ className, ...props }, ref) => (
     className={cn("relative flex w-full touch-none select-none items-center", className)}
     {...props}>
     <SliderPrimitive.Track
-      className="relative h-1.5 w-full grow overflow-hidden rounded-full bg-primary/20">
-      <SliderPrimitive.Range className="absolute h-full bg-primary" />
+      className="relative h-1.5 w-full grow overflow-hidden rounded-full bg-gray-600">
+      <SliderPrimitive.Range className="absolute h-full bg-teal-500" />
     </SliderPrimitive.Track>
     <SliderPrimitive.Thumb
-      className="block h-4 w-4 rounded-full border border-primary/50 bg-background shadow transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50" />
-  </SliderPrimitive.Root>
+      className="block h-4 w-4 rounded-full border border-teal-400 bg-white shadow transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50" />  </SliderPrimitive.Root>
 ))
 Slider.displayName = SliderPrimitive.Root.displayName
 
